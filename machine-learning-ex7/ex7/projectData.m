4ce33609f3a57a488125d158fcd81eb773fbc83b
Qian Long <qlong@alum.mit.edu>
2016-04-17 20:55:47 -0700

    Ex7 initial code and answers

---
+++ machine-learning-ex7/ex7/projectData.m
@@ -0,0 +1,31 @@
+function Z = projectData(X, U, K)
+%PROJECTDATA Computes the reduced data representation when projecting only 
+%on to the top k eigenvectors
+%   Z = projectData(X, U, K) computes the projection of 
+%   the normalized inputs X into the reduced dimensional space spanned by
+%   the first K columns of U. It returns the projected examples in Z.
+%
+
+% You need to return the following variables correctly.
+Z = zeros(size(X, 1), K);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the projection of the data using only the top K 
+%               eigenvectors in U (first K columns). 
+%               For the i-th example X(i,:), the projection on to the k-th 
+%               eigenvector is given as follows:
+%                    x = X(i, :)';
+%                    projection_k = x' * U(:, k);
+%
+
+% Top K vectors of the eigenvectors of the covariance matrix
+U_reduce = U(:, 1:K);
+
+% X * U_reduce
+% [m x n] * [n x k] = [m x k]
+Z = X * U_reduce;
+
+
+% =============================================================
+
+end
